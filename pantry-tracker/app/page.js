39e8a7afe0cf72225315f132815f2f1045e905d7
Stone Werner <stonewerner@berkeley.edu>
2024-07-31 20:20:58 -0500

    modal not centered

---
+++ pantry-tracker/app/page.js
@@ -0,0 +1,104 @@
+'use client'
+import Image from "next/image";
+import { useState, useEffect } from 'react'
+import { firestore } from '@/firebase'
+import {Box, Modal, Typography, Stack, TextField} from '@mui/material'
+//^ all of the html ish elements we use
+import {collection, deleteDoc, doc, query, getDocs} from 'firebase/firestore'
+
+export default function Home() {
+  const [inventory, setInventory] = useState([])
+  const [open, setOpen] = useState(true)
+  const [itemName, setItemName] = useState("")
+
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, 'inventory'))
+    const docs = await getDocs(snapshot)
+    const inventoryList = []
+    docs.forEach((doc) => {
+      inventoryList.push({
+        name: doc.id,
+        ...doc.data(),
+      })
+    })
+    setInventory(inventoryList)
+  }
+
+  //calls inventory whenever something changes in dependency list
+  //because it is empty, it only runs when page loads
+  useEffect(() => {
+    updateInventory()
+  }, [])
+
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
+  const removeItem = async (item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+    if (docSnap.exists()) {
+      const {quantity} = docSnap.data()
+      if (quantity === 1) {
+        await deleteDoc(docRef)
+      } else {
+        await setDoc(docRef, {quantity: quantity - 1})
+      }
+    }
+    await updateInventory()
+  }
+
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+    if (docSnap.exists()) {
+      const {quantity} = docSnap.data()
+      await setDoc(docRef, {quantity: quantity + 1})
+    } else {
+      await setDoc(docRef, {quantity: 1})
+
+    }
+    await updateInventory()
+  }
+
+
+
+  return (
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      gap={2}
+      >
+      <Modal open={open} onClose={handleClose}>
+        <Box
+        position="absolute"
+        top="50%"
+        left="50%"
+        transform="translate(-50%, -50%)"
+        width={400}
+        bgcolor="white"
+        border="2px solid #000"
+        boxShadow={24}
+        p={4}
+        display="flex"
+        flexDirection="column"
+        gap={3}
+        >
+          <Typography variant="h6">Add Item</Typography>
+          <Stack width="100%" direction="row" spacing={2}>
+            <TextField></TextField>
+
+          </Stack>
+
+        </Box>
+
+      </Modal>
+      
+      <Typography variant="h1">Inventory Management</Typography>
+      
+    </Box>
+
+  );
+}
---
+++ pantry-tracker/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCEgYDZl8G7QXPjD0QlbknBrDpUWYDP-V0",
+  authDomain: "pantry-tracker-2e65e.firebaseapp.com",
+  projectId: "pantry-tracker-2e65e",
+  storageBucket: "pantry-tracker-2e65e.appspot.com",
+  messagingSenderId: "932854987859",
+  appId: "1:932854987859:web:bc1056140c8e0796cd9aa6",
+  measurementId: "G-M071NLCKR3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+
+export {firestore}
