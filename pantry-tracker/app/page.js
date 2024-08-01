36891e8ed2000e031e3c3178ab4094094b941be1
Stone Werner <stonewerner@berkeley.edu>
2024-07-31 21:45:00 -0500

    simple CRUD app working with buttons

--- pantry-tracker/app/page.js
+++ pantry-tracker/app/page.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react'
 import { firestore } from '@/firebase'
 import {Box, Modal, Typography, Stack, TextField, Button} from '@mui/material'
 //^ all of the html ish elements we use
-import {collection, deleteDoc, doc, query, getDocs} from 'firebase/firestore'
+import {collection, deleteDoc, doc, query, getDocs, getDoc, setDoc} from 'firebase/firestore'
 
 export default function Home() {
   const [inventory, setInventory] = useState([])
@@ -119,10 +119,38 @@ export default function Home() {
         <Box width="800px" height="100px" bgcolor="#ADD8E6" alignItems="center" justifyContent="center" display="flex">
           <Typography variant="h2" color="#333">Current Inventory</Typography>
         </Box>
-      </Box>
       <Stack width="800px" height="300px" spacing={2} overflow="auto">
-        
+        {
+          inventory.map(({name, quantity}) => (
+            <Box
+            key={name}
+            width="100%"
+            minHeight="150px"
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+            bgColor="#f0f0f0"
+            padding={5}>
+              <Typography variant="h3" color="#333" textAlign="center">
+                {name.charAt(0).toUpperCase() + name.slice(1)}
+              </Typography>
+              <Typography variant="h3" color="#333" textAlign="center">
+                {quantity}
+              </Typography>
+              <Stack direction="row" spacing={2}>
+              <Button variant="outlined" onClick={()=> {
+                addItem(name)
+              }}>Add (+)</Button>
+              <Button variant="contained" onClick={()=> {
+                removeItem(name)
+              }}>Remove (-)</Button>
+            </Stack></Box>
+            
+        ))
+        }
+
       </Stack>
+      </Box>
     </Box>
 
   );
